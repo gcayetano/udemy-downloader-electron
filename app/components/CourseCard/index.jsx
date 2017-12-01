a88336b1b7dbe035e7d4716cea736633ba44fba9
Gabriel Cayetano <gcayetanonovalio@gmail.com>
2017-12-01 22:28:17 +0100

    CourseCard component

--- app/components/CourseCard/index.jsx
+++ app/components/CourseCard/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import {Row, Col} from 'react-bootstrap';
+import {Row, Col, Button, Glyphicon} from 'react-bootstrap';
 import styles from './style.css';
 
 const cCard = (
-    
+    <div className={styles.card}>
+        <Row bsClass={styles.cardImage}>
+			{/* Card Image */}
+        </Row>
+        <Row bsClass={styles.cardTitle}>
+			{/* Card Title */}
+			<span>[TITLE]</span>
+        </Row>
+        <Row bsClass={styles.cardDesc}>
+			{/* Card Description */}
+			<span>[Description]</span>
+        </Row>
+		<Row bsClass={styles.cardBtn}>
+			{/* Card Download Button */}
+			<Button className="pull-right"><Glyphicon glyph="circle-arrow-down" /></Button>
+        </Row>
+    </div>
 );
 
 const CourseCard = (props) => {
