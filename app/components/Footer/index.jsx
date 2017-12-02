2f8981a90ce87285c321ff359fc485bc6464d630
Gabriel Cayetano <gcayetanonovalio@gmail.com>
2017-12-02 15:34:38 +0100

    Added Footer component

---
+++ app/components/Footer/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Row, Col, Button, Glyphicon} from 'react-bootstrap';
+import styles from './style.css';
+
+const Footer = (props) => {
+	return (
+		<footer className={styles.footer}>
+            <Row>
+                <Col md={6}>
+                    <span className={styles.copy}>Copyright 2017 - gcayetano</span>
+                </Col>
+                <Col md={6}>
+                    <span className="pull-right">Sharing is the key</span>
+                </Col>
+            </Row>
+        </footer>
+	);
+}
+  
+export default Footer;
